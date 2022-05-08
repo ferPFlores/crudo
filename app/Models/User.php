9dff9b0c0b6544f5765c26a216a742dd9ed4ff50
Fernando Peña <fer.dogs99@gmail.com>
2022-05-08 17:57:08 -0500

    Agregado de campos para registro y campo para mostrar nombre al estar logeado

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -41,4 +42,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function getFullname(){
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
