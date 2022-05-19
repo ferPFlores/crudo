ba16429bdbe35301ad089f5a1d4ce285fd5eb5ec
Cesar Larios <cesar.larios2963@alumnos.udg.mx>
2022-05-19 16:26:59 -0500

    lista de cliente, crear, actualizar y eliminar

---
+++ app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'address',
+        'avatar',
+        'user_id',
+    ];
+
+    public function getAvatarUrl()
+    {
+        return storage::url($this->avatar);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,7 +9,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     Route::resource('products', 'ProductController');
+    Route::resource('customers', 'CustomerController');
 });
